504b263adeb6ac9a39e667276ace73be5663cce6
Richard Walters <rwalters@digitalstirling.com>
2019-12-13 23:10:09 -0800

    Add solution for 2019 puzzle 9-2

---
+++ 2019/9-2/src/main.cpp
@@ -0,0 +1,285 @@
+/**
+ * @file main.cpp
+ *
+ * This module holds the main() function, which is the entrypoint
+ * to the program.
+ *
+ * © 2019 by Richard Walters
+ */
+
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <inttypes.h>
+#include <memory>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#ifdef _WIN32
+#include <crtdbg.h>
+#endif /* _WIN32 */
+
+intmax_t GetNextNumber(
+    const std::string& input,
+    size_t& pos
+) {
+    auto delimiter = input.find(',', pos);
+    if (delimiter == std::string::npos) {
+        delimiter = input.length();
+    }
+    intmax_t number;
+    if (sscanf(input.substr(pos, delimiter - pos).c_str(), "%" SCNdMAX, &number) != 1) {
+        (void)fprintf(stderr, "Bad input detected at position %zu\n", pos);
+        exit(1);
+    }
+    pos = delimiter + 1;
+    return number;
+}
+
+struct Machine {
+    size_t id = 0;
+    size_t pos = 0;
+    std::vector< intmax_t > numbers;
+    std::vector< intmax_t > input;
+    bool halted = false;
+    intmax_t relativeBase = 0;
+
+    void ExpandToFit(size_t index) {
+        if (index >= numbers.size()) {
+            numbers.resize(index + 1);
+        }
+    }
+
+    size_t LoadIndex(
+        size_t pos,
+        int mode
+    ) {
+        switch (mode) {
+            case 0: { // position
+                const auto index = (size_t)numbers[pos];
+                return index;
+            } break;
+
+            case 2: { // relative
+                const auto offset = numbers[pos];
+                const auto index = (size_t)(relativeBase + offset);
+                return index;
+            } break;
+
+            default: {
+                (void)fprintf(stderr, "Invalid index mode for offset %zu\n", pos);
+                exit(1);
+            }
+        }
+        return 0;
+    }
+
+    intmax_t LoadArgument(
+        size_t pos,
+        int mode
+    ) {
+        switch (mode) {
+            case 0: { // position
+                const auto index = (size_t)numbers[pos];
+                ExpandToFit(index);
+                const auto arg = numbers[index];
+                return arg;
+            } break;
+
+            case 1: { // immediate
+                const auto arg = numbers[pos];
+                return arg;
+            } break;
+
+            case 2: { // relative
+                const auto offset = numbers[pos];
+                const auto index = (size_t)(relativeBase + offset);
+                ExpandToFit(index);
+                const auto arg = numbers[index];
+                return arg;
+            } break;
+
+            default: {
+                (void)fprintf(stderr, "Invalid argument mode for offset %zu\n", pos);
+                exit(1);
+            }
+        }
+        return 0;
+    }
+
+    void Store(
+        size_t index,
+        intmax_t value
+    ) {
+        ExpandToFit(index);
+        numbers[index] = value;
+    }
+
+    void Run(std::vector< intmax_t >& output) {
+        printf("*** Machine %zu Run ***\n", id);
+        while (!halted) {
+            const auto opcode = numbers[pos] % 100;
+            switch (opcode) {
+                case 1: { // add
+                    const auto arg1 = LoadArgument(pos + 1, (numbers[pos] / 100) % 10);
+                    const auto arg2 = LoadArgument(pos + 2, (numbers[pos] / 1000) % 10);
+                    const auto index3 = LoadIndex(pos + 3, (numbers[pos] / 10000) % 10);
+                    Store(index3, arg1 + arg2);
+                    pos += 4;
+                } break;
+
+                case 2: { // multiply
+                    const auto arg1 = LoadArgument(pos + 1, (numbers[pos] / 100) % 10);
+                    const auto arg2 = LoadArgument(pos + 2, (numbers[pos] / 1000) % 10);
+                    const auto index3 = LoadIndex(pos + 3, (numbers[pos] / 10000) % 10);
+                    Store(index3, arg1 * arg2);
+                    pos += 4;
+                } break;
+
+                case 3: { // input
+                    const auto index = LoadIndex(pos + 1, (numbers[pos] / 100) % 10);
+                    if (input.empty()) {
+                        printf("*** Machine %zu Needs Input ***\n", id);
+                        return;
+                    }
+                    const auto inputValue = input[0];
+                    (void)input.erase(input.begin());
+                    Store(index, inputValue);
+                    pos += 2;
+                } break;
+
+                case 4: { // output
+                    const auto outputValue = LoadArgument(pos + 1, (numbers[pos] / 100) % 10);
+                    output.push_back(outputValue);
+                    pos += 2;
+                } break;
+
+                case 5: { // jump-if-true
+                    const auto arg1 = LoadArgument(pos + 1, (numbers[pos] / 100) % 10);
+                    const auto arg2 = (size_t)LoadArgument(pos + 2, (numbers[pos] / 1000) % 10);
+                    if (arg1 != 0) {
+                        pos = arg2;
+                    } else {
+                        pos += 3;
+                    }
+                } break;
+
+                case 6: { // jump-if-false
+                    const auto arg1 = LoadArgument(pos + 1, (numbers[pos] / 100) % 10);
+                    const auto arg2 = (size_t)LoadArgument(pos + 2, (numbers[pos] / 1000) % 10);
+                    if (arg1 == 0) {
+                        pos = arg2;
+                    } else {
+                        pos += 3;
+                    }
+                } break;
+
+                case 7: { // less-than
+                    const auto arg1 = LoadArgument(pos + 1, (numbers[pos] / 100) % 10);
+                    const auto arg2 = LoadArgument(pos + 2, (numbers[pos] / 1000) % 10);
+                    const auto index3 = LoadIndex(pos + 3, (numbers[pos] / 10000) % 10);
+                    Store(
+                        index3,
+                        (
+                            (arg1 < arg2)
+                            ? 1
+                            : 0
+                        )
+                    );
+                    pos += 4;
+                } break;
+
+                case 8: { // equals
+                    const auto arg1 = LoadArgument(pos + 1, (numbers[pos] / 100) % 10);
+                    const auto arg2 = LoadArgument(pos + 2, (numbers[pos] / 1000) % 10);
+                    const auto index3 = LoadIndex(pos + 3, (numbers[pos] / 10000) % 10);
+                    Store(
+                        index3,
+                        (
+                            (arg1 == arg2)
+                            ? 1
+                            : 0
+                        )
+                    );
+                    pos += 4;
+                } break;
+
+                case 9: { // adjust relative base
+                    const auto arg1 = LoadArgument(pos + 1, (numbers[pos] / 100) % 10);
+                    relativeBase += arg1;
+                    pos += 2;
+                } break;
+
+                case 99: { // stop
+                    halted = true;
+                } break;
+
+                default: { // ERROR!!
+                    (void)fprintf(stderr, "Invalid opcode (%" PRIdMAX ")\n", opcode);
+                    exit(1);
+                } break;
+            }
+        }
+        printf("*** Machine %zu Halted ***\n", id);
+    }
+};
+
+/**
+ * This function is the entrypoint of the program.
+ *
+ * @param[in] argc
+ *     This is the number of command-line arguments given to the program.
+ *
+ * @param[in] argv
+ *     This is the array of command-line arguments given to the program.
+ */
+int main(int argc, char* argv[]) {
+#ifdef _WIN32
+    //_crtBreakAlloc = 18;
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif /* _WIN32 */
+    (void)setbuf(stdout, NULL);
+
+    // Open the input file and read in the input string.
+    std::ifstream input("input.txt");
+    std::string line;
+    (void)std::getline(input, line);
+
+    // Parse the input string into a vector of numbers.
+    size_t pos = 0;
+    const auto inputLength = line.length();
+    std::vector< intmax_t > numbers;
+    while (pos < inputLength) {
+        const auto number = GetNextNumber(line, pos);
+        numbers.push_back(number);
+    }
+
+    // Load machine with input.
+    Machine machine;
+    machine.id = 1;
+    machine.numbers = std::move(numbers);
+    printf("Input: ");
+    intmax_t inputValue;
+    if (scanf("%" SCNdMAX, &inputValue) != 1) {
+        (void)fprintf(stderr, "Invalid input\n");
+        exit(1);
+    }
+    machine.input.push_back(inputValue);
+
+    // Run the machine and print its output.
+    std::vector< intmax_t > output;
+    machine.Run(output);
+    printf("Output: ");
+    bool first = true;
+    for (auto value: output) {
+        if (!first) {
+            printf(", ");
+        }
+        first = false;
+        printf("%" PRIdMAX, value);
+    }
+    printf("\n");
+    return EXIT_SUCCESS;
+}
