c989ce07de83c7267b3cd8b4c30a372cf805be12
Richard Walters <rwalters@digitalstirling.com>
2020-11-30 21:34:17 -0800

    Add solutions to 2020 day 1

--- 2020/src/bin/01-1.rs
+++ 2020/src/bin/01-1.rs
@@ -1,3 +1,23 @@
+use std::{
+    collections::HashSet,
+    io::{
+        stdin,
+        Read,
+    },
+};
+
 fn main() {
-    println!("Hello, World!");
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let nums = input
+        .lines()
+        .map(|line| line.trim().parse().unwrap())
+        .collect::<HashSet<u32>>();
+    for num in &nums {
+        let complement = 2020 - num;
+        if nums.contains(&complement) {
+            println!("{}", num * complement);
+            return;
+        }
+    }
 }
--- 2020/src/bin/01-2.rs
+++ 2020/src/bin/01-2.rs
@@ -1,3 +1,29 @@
+use std::{
+    collections::HashSet,
+    io::{
+        stdin,
+        Read,
+    },
+};
+
 fn main() {
-    println!("Hello, World!");
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let nums = input
+        .lines()
+        .map(|line| line.trim().parse().unwrap())
+        .collect::<HashSet<u32>>();
+    for &num1 in &nums {
+        let remainder = 2020 - num1;
+        for &num2 in &nums {
+            if num2 == num1 || num2 > remainder {
+                continue;
+            }
+            let num3 = 2020 - num1 - num2;
+            if nums.contains(&num3) {
+                println!("{}", num1 * num2 * num3);
+                return;
+            }
+        }
+    }
 }
