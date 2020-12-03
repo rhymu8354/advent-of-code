baefbf1d4132a0bd931583a0ab0e9cab9d76dd9a
Richard Walters <rwalters@digitalstirling.com>
2020-12-02 22:02:37 -0800

    Add benchmarking for 2020 days 1 and 2

--- 2020/src/bin/01-2.rs
+++ 2020/src/bin/01-2.rs
@@ -4,26 +4,81 @@ use std::{
         stdin,
         Read,
     },
+    time::{
+        Duration,
+        Instant,
+    },
 };
 
-fn main() {
-    let mut input = String::new();
-    stdin().read_to_string(&mut input).unwrap();
-    let nums = input
+fn parse(input: &str) -> HashSet<u32> {
+    input
         .lines()
         .map(|line| line.trim().parse().unwrap())
-        .collect::<HashSet<u32>>();
-    for &num1 in &nums {
+        .collect::<HashSet<u32>>()
+}
+
+fn solve_already_parsed(nums: &HashSet<u32>) -> Option<u32> {
+    for &num1 in nums {
         let remainder = 2020 - num1;
-        for &num2 in &nums {
+        for &num2 in nums {
             if num2 == num1 || num2 > remainder {
                 continue;
             }
             let num3 = 2020 - num1 - num2;
             if nums.contains(&num3) {
-                println!("{}", num1 * num2 * num3);
-                return;
+                return Some(num1 * num2 * num3);
             }
         }
     }
+    None
+}
+
+fn solve(input: &str) -> Option<u32> {
+    solve_already_parsed(&parse(input))
+}
+
+const ITERATIONS: usize = 100000;
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let solution = solve(&input).unwrap();
+    println!("Solution: {}", solution);
+    let mut overhead = Duration::default();
+    for _ in 0..ITERATIONS {
+        let start = Instant::now();
+        let elapsed = start.elapsed();
+        overhead += elapsed;
+    }
+    let mut total = Duration::default();
+    for _ in 0..ITERATIONS {
+        let start = Instant::now();
+        let solution_repeat = solve(&input).unwrap();
+        assert_eq!(solution_repeat, solution);
+        let elapsed = start.elapsed();
+        total += elapsed;
+    }
+    let map = parse(&input);
+    let mut total_without_parsing = Duration::default();
+    for _ in 0..ITERATIONS {
+        let start = Instant::now();
+        let solution_repeat = solve_already_parsed(&map).unwrap();
+        assert_eq!(solution_repeat, solution);
+        let elapsed = start.elapsed();
+        total_without_parsing += elapsed;
+    }
+    println!("Time (with parsing): {:?}", total / ITERATIONS as u32);
+    println!(
+        "Time (without parsing): {:?}",
+        total_without_parsing / ITERATIONS as u32
+    );
+    println!("Overhead: {:?}", overhead / ITERATIONS as u32);
+    println!(
+        "Time (with parsing) - Overhead: {:?}",
+        (total - overhead) / ITERATIONS as u32
+    );
+    println!(
+        "Time (without parsing) - Overhead: {:?}",
+        (total_without_parsing - overhead) / ITERATIONS as u32
+    );
 }
--- 2020/src/bin/02-2.rs
+++ 2020/src/bin/02-2.rs
@@ -1,6 +1,12 @@
-use std::io::{
-    stdin,
-    Read,
+use std::{
+    io::{
+        stdin,
+        Read,
+    },
+    time::{
+        Duration,
+        Instant,
+    },
 };
 
 struct PasswordWithPolicy<'a> {
@@ -62,13 +68,63 @@ fn is_valid_password(
         == 1
 }
 
+fn parse(input: &str) -> Vec<PasswordWithPolicy> {
+    input.lines().map(|line| scan(line.trim())).collect()
+}
+
+fn solve_already_parsed(input: &[PasswordWithPolicy]) -> usize {
+    input
+        .iter()
+        .filter(|password_with_policy| is_valid_password(password_with_policy))
+        .count()
+}
+
+fn solve(input: &str) -> usize {
+    solve_already_parsed(&parse(input))
+}
+
+const ITERATIONS: usize = 10000;
+
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
-    let valid_passwords = input
-        .lines()
-        .map(|line| scan(line.trim()))
-        .filter(|password_with_policy| is_valid_password(password_with_policy))
-        .count();
-    println!("There are {} valid passwords.", valid_passwords);
+    let solution = solve(&input);
+    println!("Solution: {}", solution);
+    let mut overhead = Duration::default();
+    for _ in 0..ITERATIONS {
+        let start = Instant::now();
+        let elapsed = start.elapsed();
+        overhead += elapsed;
+    }
+    let mut total = Duration::default();
+    for _ in 0..ITERATIONS {
+        let start = Instant::now();
+        let solution_repeat = solve(&input);
+        assert_eq!(solution_repeat, solution);
+        let elapsed = start.elapsed();
+        total += elapsed;
+    }
+    let map = parse(&input);
+    let mut total_without_parsing = Duration::default();
+    for _ in 0..ITERATIONS {
+        let start = Instant::now();
+        let solution_repeat = solve_already_parsed(&map);
+        assert_eq!(solution_repeat, solution);
+        let elapsed = start.elapsed();
+        total_without_parsing += elapsed;
+    }
+    println!("Time (with parsing): {:?}", total / ITERATIONS as u32);
+    println!(
+        "Time (without parsing): {:?}",
+        total_without_parsing / ITERATIONS as u32
+    );
+    println!("Overhead: {:?}", overhead / ITERATIONS as u32);
+    println!(
+        "Time (with parsing) - Overhead: {:?}",
+        (total - overhead) / ITERATIONS as u32
+    );
+    println!(
+        "Time (without parsing) - Overhead: {:?}",
+        (total_without_parsing - overhead) / ITERATIONS as u32
+    );
 }
