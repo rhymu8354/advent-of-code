9e646cf02ca86f4dfcd3fff43fa0107ba49ad6e3
Richard Walters <rwalters@digitalstirling.com>
2020-12-03 22:29:44 -0800

    Use standard generic benchmarking for 2020 day 2

--- 2020/src/bin/02-1.rs
+++ 2020/src/bin/02-1.rs
@@ -1,19 +1,10 @@
-use std::{
-    io::{
-        stdin,
-        Read,
-    },
-    time::{
-        Duration,
-        Instant,
-    },
-};
+use aoc_2020::benchmarked_main;
 
-struct PasswordWithPolicy<'a> {
+struct PasswordWithPolicy {
     min: usize,
     max: usize,
     ch: char,
-    password: &'a str,
+    password: String,
 }
 
 fn chomp(
@@ -44,7 +35,7 @@ fn scan(line: &str) -> PasswordWithPolicy {
     let (max, line) = chomp_number(line, ' ');
     let ch = line.chars().next().unwrap();
     let password_start = ch.len_utf8() + ": ".len();
-    let password = &line[password_start..];
+    let password = String::from(&line[password_start..]);
     PasswordWithPolicy {
         min,
         max,
@@ -70,59 +61,22 @@ fn parse(input: &str) -> Vec<PasswordWithPolicy> {
     input.lines().map(|line| scan(line.trim())).collect()
 }
 
-fn solve_already_parsed(input: &[PasswordWithPolicy]) -> usize {
-    input
-        .iter()
-        .filter(|password_with_policy| is_valid_password(password_with_policy))
-        .count()
-}
-
-fn solve(input: &str) -> usize {
-    solve_already_parsed(&parse(input))
+fn solve_already_parsed<T: AsRef<[PasswordWithPolicy]>>(
+    input: &T
+) -> Option<usize> {
+    let input = input.as_ref();
+    Some(
+        input
+            .iter()
+            .filter(|password_with_policy| {
+                is_valid_password(password_with_policy)
+            })
+            .count(),
+    )
 }
 
 const ITERATIONS: usize = 10000;
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_to_string(&mut input).unwrap();
-    let solution = solve(&input);
-    println!("Solution: {}", solution);
-    let mut overhead = Duration::default();
-    for _ in 0..ITERATIONS {
-        let start = Instant::now();
-        let elapsed = start.elapsed();
-        overhead += elapsed;
-    }
-    let mut total = Duration::default();
-    for _ in 0..ITERATIONS {
-        let start = Instant::now();
-        let solution_repeat = solve(&input);
-        assert_eq!(solution_repeat, solution);
-        let elapsed = start.elapsed();
-        total += elapsed;
-    }
-    let map = parse(&input);
-    let mut total_without_parsing = Duration::default();
-    for _ in 0..ITERATIONS {
-        let start = Instant::now();
-        let solution_repeat = solve_already_parsed(&map);
-        assert_eq!(solution_repeat, solution);
-        let elapsed = start.elapsed();
-        total_without_parsing += elapsed;
-    }
-    println!("Time (with parsing): {:?}", total / ITERATIONS as u32);
-    println!(
-        "Time (without parsing): {:?}",
-        total_without_parsing / ITERATIONS as u32
-    );
-    println!("Overhead: {:?}", overhead / ITERATIONS as u32);
-    println!(
-        "Time (with parsing) - Overhead: {:?}",
-        (total - overhead) / ITERATIONS as u32
-    );
-    println!(
-        "Time (without parsing) - Overhead: {:?}",
-        (total_without_parsing - overhead) / ITERATIONS as u32
-    );
+    benchmarked_main(parse, solve_already_parsed, ITERATIONS);
 }
