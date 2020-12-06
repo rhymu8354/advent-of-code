d83b2f97843708642b7b316190356bd6947a0079
Richard Walters <rwalters@digitalstirling.com>
2020-12-05 21:31:52 -0800

    Add solutions to 2020 day 6

--- 2020/src/bin/06-1.rs
+++ 2020/src/bin/06-1.rs
@@ -1,34 +1,29 @@
+use std::collections::HashSet;
+
 use aoc_2020::benchmarked_main;
 
-fn parse(input: &str) -> Vec<Vec<char>> {
-    input
-        .lines()
-        .map(|line| line.trim().chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>()
-}
-
-const SLOPE_X: usize = 3;
-
-fn try_path(
-    map: &[Vec<char>],
-    cols: usize,
-) -> usize {
-    map.iter()
-        .enumerate()
-        .filter(|(i, row)| row[(i * SLOPE_X) % cols] == '#')
-        .count()
-}
-
-fn solve_already_parsed<T: AsRef<[Vec<char>]>>(map: &T) -> Option<usize> {
-    let map = map.as_ref();
-    if let Some(row) = map.get(0) {
-        Some(try_path(map, row.len()))
-    } else {
-        None
+fn parse(input: &str) -> Vec<HashSet<char>> {
+    let mut groups = Vec::new();
+    let mut group = HashSet::new();
+    for line in input.lines().map(|line| line.trim()) {
+        if line.is_empty() {
+            let mut new_group = HashSet::new();
+            std::mem::swap(&mut group, &mut new_group);
+            groups.push(new_group);
+        } else {
+            group.extend(line.chars());
+        }
     }
+    groups.push(group);
+    groups
 }
 
-const ITERATIONS: usize = 10000;
+fn solve_already_parsed<T: AsRef<[HashSet<char>]>>(input: &T) -> Option<usize> {
+    let input = input.as_ref();
+    Some(input.iter().fold(0, |acc, group| acc + group.len()))
+}
+
+const ITERATIONS: usize = 1000;
 
 fn main() {
     benchmarked_main(parse, solve_already_parsed, ITERATIONS);
--- 2020/src/bin/06-2.rs
+++ 2020/src/bin/06-2.rs
@@ -1,34 +1,39 @@
+use std::collections::HashSet;
+
 use aoc_2020::benchmarked_main;
 
-fn parse(input: &str) -> Vec<Vec<char>> {
-    input
-        .lines()
-        .map(|line| line.trim().chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>()
-}
-
-const SLOPE_X: usize = 3;
-
-fn try_path(
-    map: &[Vec<char>],
-    cols: usize,
-) -> usize {
-    map.iter()
-        .enumerate()
-        .filter(|(i, row)| row[(i * SLOPE_X) % cols] == '#')
-        .count()
-}
-
-fn solve_already_parsed<T: AsRef<[Vec<char>]>>(map: &T) -> Option<usize> {
-    let map = map.as_ref();
-    if let Some(row) = map.get(0) {
-        Some(try_path(map, row.len()))
-    } else {
-        None
+fn parse(input: &str) -> Vec<Vec<HashSet<char>>> {
+    let mut groups = Vec::new();
+    let mut group = Vec::new();
+    for line in input.lines().map(|line| line.trim()) {
+        if line.is_empty() {
+            let mut new_group = Vec::new();
+            std::mem::swap(&mut group, &mut new_group);
+            groups.push(new_group);
+        } else {
+            group.push(line.chars().collect::<HashSet<char>>());
+        }
     }
+    groups.push(group);
+    groups
 }
 
-const ITERATIONS: usize = 10000;
+fn solve_already_parsed<T: AsRef<[Vec<HashSet<char>>]>>(
+    input: &T
+) -> Option<usize> {
+    let input = input.as_ref();
+    Some(input.iter().fold(0, |acc, group| {
+        let mut group = group.iter();
+        let first = group.next().unwrap();
+        acc + group
+            .fold(first.clone(), |acc, person| {
+                acc.intersection(person).copied().collect::<HashSet<char>>()
+            })
+            .len()
+    }))
+}
+
+const ITERATIONS: usize = 1000;
 
 fn main() {
     benchmarked_main(parse, solve_already_parsed, ITERATIONS);
