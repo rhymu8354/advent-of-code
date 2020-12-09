99f22676ec9a07bc6896a02efa5fe952ea8816b1
Richard Walters <rwalters@digitalstirling.com>
2020-12-08 22:22:13 -0800

    Add solutions to 2020 day 9

--- 2020/src/bin/09-1.rs
+++ 2020/src/bin/09-1.rs
@@ -1,34 +1,39 @@
 use aoc_2020::benchmarked_main;
+use std::collections::HashSet;
 
-fn parse(input: &str) -> Vec<Vec<char>> {
-    input
-        .lines()
-        .map(|line| line.trim().chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>()
+const WINDOW_LENGTH: usize = 25;
+
+fn parse(input: &str) -> Vec<usize> {
+    input.lines().map(|line| line.parse().unwrap()).collect::<Vec<_>>()
 }
 
-const SLOPE_X: usize = 3;
-
-fn try_path(
-    map: &[Vec<char>],
-    cols: usize,
-) -> usize {
-    map.iter()
-        .enumerate()
-        .filter(|(i, row)| row[(i * SLOPE_X) % cols] == '#')
-        .count()
+fn is_valid<T: AsRef<[usize]>>(
+    input: T,
+    window_contents: &HashSet<usize>,
+    total: usize,
+) -> bool {
+    input.as_ref().iter().any(|&lhs| {
+        let rhs = total - lhs;
+        lhs != rhs && window_contents.contains(&rhs)
+    })
 }
 
-fn solve_already_parsed<T: AsRef<[Vec<char>]>>(map: &T) -> Option<usize> {
-    let map = map.as_ref();
-    if let Some(row) = map.get(0) {
-        Some(try_path(map, row.len()))
-    } else {
-        None
-    }
+fn solve_already_parsed<T: AsRef<[usize]>>(input: &T) -> Option<usize> {
+    let input = input.as_ref();
+    let mut window_contents =
+        input.iter().take(WINDOW_LENGTH).copied().collect::<HashSet<_>>();
+    input[WINDOW_LENGTH..].iter().enumerate().find_map(|(i, &total)| {
+        if is_valid(&input[i..i + WINDOW_LENGTH], &window_contents, total) {
+            window_contents.remove(&input[i]);
+            window_contents.insert(total);
+            None
+        } else {
+            Some(total)
+        }
+    })
 }
 
-const ITERATIONS: usize = 10000;
+const ITERATIONS: usize = 1;
 
 fn main() {
     benchmarked_main(parse, solve_already_parsed, ITERATIONS);
--- 2020/src/bin/09-2.rs
+++ 2020/src/bin/09-2.rs
@@ -1,31 +1,69 @@
 use aoc_2020::benchmarked_main;
+use std::{
+    cmp::Ordering,
+    collections::HashSet,
+};
 
-fn parse(input: &str) -> Vec<Vec<char>> {
-    input
-        .lines()
-        .map(|line| line.trim().chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>()
+const WINDOW_LENGTH: usize = 25;
+
+fn parse(input: &str) -> Vec<usize> {
+    input.lines().map(|line| line.parse().unwrap()).collect::<Vec<_>>()
 }
 
-const SLOPE_X: usize = 3;
-
-fn try_path(
-    map: &[Vec<char>],
-    cols: usize,
-) -> usize {
-    map.iter()
-        .enumerate()
-        .filter(|(i, row)| row[(i * SLOPE_X) % cols] == '#')
-        .count()
+fn is_valid<T: AsRef<[usize]>>(
+    input: T,
+    window_contents: &HashSet<usize>,
+    total: usize,
+) -> bool {
+    input.as_ref().iter().any(|&lhs| {
+        let rhs = total - lhs;
+        lhs != rhs && window_contents.contains(&rhs)
+    })
 }
 
-fn solve_already_parsed<T: AsRef<[Vec<char>]>>(map: &T) -> Option<usize> {
-    let map = map.as_ref();
-    if let Some(row) = map.get(0) {
-        Some(try_path(map, row.len()))
-    } else {
-        None
+fn find_invalid_number<T: AsRef<[usize]>>(input: &T) -> Option<usize> {
+    let input = input.as_ref();
+    let mut window_contents =
+        input.iter().take(WINDOW_LENGTH).copied().collect::<HashSet<_>>();
+    input[WINDOW_LENGTH..].iter().enumerate().find_map(|(i, &total)| {
+        if is_valid(&input[i..i + WINDOW_LENGTH], &window_contents, total) {
+            window_contents.remove(&input[i]);
+            window_contents.insert(total);
+            None
+        } else {
+            Some(total)
+        }
+    })
+}
+
+fn solve_already_parsed<T: AsRef<[usize]>>(input: &T) -> Option<usize> {
+    let input = input.as_ref();
+    let invalid_number = find_invalid_number(&input).unwrap();
+    let mut first = 0;
+    let mut last = 0;
+    let mut sum = 0;
+    while last < input.len() {
+        match sum.cmp(&invalid_number) {
+            Ordering::Less => {
+                sum += input[last];
+                last += 1;
+            },
+            Ordering::Equal => {
+                let mut window =
+                    input.iter().skip(first).take(last - first).copied();
+                let first = window.next()?;
+                let (min, max) = window.fold((first, first), |acc, next| {
+                    (acc.0.min(next), acc.1.max(next))
+                });
+                return Some(min + max);
+            },
+            Ordering::Greater => {
+                sum -= input[first];
+                first += 1;
+            },
+        }
     }
+    None
 }
 
 const ITERATIONS: usize = 10000;
